5cb809c5e320414995fb59ea3a5fbbc188ecbeec
bashirulazam <35898892+bashirulazam@users.noreply.github.com>
2023-03-10 13:04:37 -0500

    Delete Post_infer_evi_prior directory

--- Post_infer_evi_prior/xywh_to_xyxy.m
+++
@@ -1,8 +0,0 @@
-function bbox_o = xywh_to_xyxy(bbox_in)
-
-bbox_o(:,1) = floor(max(bbox_in(:,1) - bbox_in(:,3)/2,0));
-bbox_o(:,2) = floor(max(bbox_in(:,2) - bbox_in(:,4)/2,0));
-% bbox_o(:,3) = round(bbox_in(:,1) + bbox_in(:,3)/2);
-% bbox_o(:,4) = round(bbox_in(:,2) + bbox_in(:,4)/2);
-bbox_o(:,3) = bbox_o(:,1) + max(0,bbox_in(:,3));
-bbox_o(:,4) = bbox_o(:,2) + max(0,bbox_in(:,4));
