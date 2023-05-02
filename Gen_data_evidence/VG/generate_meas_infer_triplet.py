9d9646969ef4cb22cb20226ec3d61c50e8f99bf3
bashirulazam <35898892+bashirulazam@users.noreply.github.com>
2023-05-02 16:59:32 -0400

    Add files via upload

--- Gen_data_evidence/VG/generate_meas_infer_triplet.py
+++ Gen_data_evidence/VG/generate_meas_infer_triplet.py
@@ -2,9 +2,11 @@ import sys
 import torch
 import numpy as np
 import scipy.io as sio
-import matplotlib.pyplot as plt
 np.set_printoptions(threshold=sys.maxsize)
-results = torch.load('./checkpoints/trained_vctree_predcls/inference/VG_stanford_filtered_with_attribute_test/eval_results.pytorch')
+
+results = torch.load('checkpoints/trained_vctree_sgdet/inference/VG_stanford_filtered_with_attribute_test/eval_results.pytorch')
+
+
 predictions = results['predictions']
 M = len(predictions)
 triplets_meas_list = []
@@ -17,22 +19,22 @@ triplets_box_list =[]
 relations_list = []
 obj_logits_list = []
 predicate_logits_list = []
-range_start = 0
-range_end = 26446
-indices = np.arange(range_start,range_end)
-method = 'vctree_'
-setting = 'predcls_'
+indices = np.arange(0, M)
+model = 'vctree'
+setting = 'sgdet'
+dataset = 'vg' 
+suffix = model + '_' + setting + '_' + dataset 
+
 for i in indices:
     print(i)
     prediction = predictions[i]
-    #prediction_pred = prediction_preds[i]
     image_width = prediction.size[0]
     image_height = prediction.size[1]
     obj_rois = prediction.bbox
     obj_scores = prediction.extra_fields['pred_scores'] 
     obj_labels = prediction.extra_fields['pred_labels']
     obj_logits = prediction.extra_fields['pred_obj_prob']
-    obj_logits = np.float16(obj_logits.numpy()[:,1:])
+    obj_logits = np.float16(obj_logits.numpy()[:, 1:])
     rel_inds = prediction.extra_fields['rel_pair_idxs']
     rel_scores = prediction.extra_fields['pred_rel_scores']
     obj_rois = obj_rois.cpu()
@@ -51,10 +53,9 @@ for i in indices:
     relations = rel_inds.numpy()
     num_relations = relations.shape[0]
     classes = obj_labels.numpy()
-    #classes = mylabels
     class_scores = obj_scores.numpy()
-    subs = classes[relations[:,0]]
-    objs = classes[relations[:,1]]
+    subs = classes[relations[:, 0]]
+    objs = classes[relations[:, 1]]
     boxes = box_preds
     assert(predicates_meas.shape[0] == relations.shape[0])
     triplets_meas = np.zeros([num_relations, 3]).astype(np.int32)
@@ -64,31 +65,30 @@ for i in indices:
     triplet_scores_prod = np.zeros([num_relations]).astype(np.float32)
     for j in range(num_relations):
         triplets_meas[j, 1] = predicates_meas[j]
-        sub_j, obj_j = relations[j,:2]
+        sub_j, obj_j = relations[j, :2]
         triplets_meas[j, 0] = classes[sub_j]
         triplets_meas[j, 2] = classes[obj_j]
         triplet_boxes[j, :4] = boxes[sub_j, :]
         triplet_boxes[j, 4:] = boxes[obj_j, :]
-        triplet_scores[j,0] =  class_scores[sub_j]
-        triplet_scores[j,2] = class_scores[obj_j]
-        triplet_scores[j,1] = predicate_scores[j]
+        triplet_scores[j, 0] =  class_scores[sub_j]
+        triplet_scores[j, 2] = class_scores[obj_j]
+        triplet_scores[j, 1] = predicate_scores[j]
         triplet_scores_prod[j] = class_scores[sub_j]*class_scores[obj_j]*predicate_scores[j]
     sorted_triplet_scores_inds = np.argsort(triplet_scores_prod)[::-1]
     sorted_triplet_scores_inds = sorted_triplet_scores_inds[triplet_scores_prod[sorted_triplet_scores_inds] > 0 ]
-    #for j, index in enumerate(sorted_triplet_scores_inds):
-        #print(triplets[index])
+
     print("Appending " + str(i))
-    top_K = np.minimum(num_relations,992)
+    top_K = np.minimum(num_relations,100)
     sorted_triplet_scores_inds = sorted_triplet_scores_inds[:top_K]
-    triplets_meas = triplets_meas[sorted_triplet_scores_inds,:]
-    triplet_boxes  = triplet_boxes[sorted_triplet_scores_inds,:]
+    triplets_meas = triplets_meas[sorted_triplet_scores_inds, :]
+    triplet_boxes  = triplet_boxes[sorted_triplet_scores_inds, :]
     triplet_scores = triplet_scores[sorted_triplet_scores_inds]
-    relations = relations[sorted_triplet_scores_inds,:]
-    predicate_logits = predicate_logits[sorted_triplet_scores_inds,:]
+    relations = relations[sorted_triplet_scores_inds, :]
+    predicate_logits = predicate_logits[sorted_triplet_scores_inds, :]
 
     triplets_meas_list.append(triplets_meas)
     triplets_scores_list.append(triplet_scores)
-    labels_list.append(np.reshape(classes,(classes.shape[0],-1)))
+    labels_list.append(np.reshape(classes, (classes.shape[0], -1)))
     score_list.append(np.reshape(class_scores,(class_scores.shape[0],-1)))
     boxes_list.append(box_preds)
     triplets_box_list.append(triplet_boxes)
@@ -98,13 +98,37 @@ for i in indices:
     predicate_logits_list.append(predicate_logits)
 
 
-range_start = range_start + 1 #For MATLAB Saving 
-range_end = range_end
 
 
-im_range = str(range_start) +'_' + str(range_end)
-im_range = 'full'
-save_file = 'data_rel_meas_infer_' + method + setting + 'vg_' + im_range + '.mat'
 
+if setting == 'sgdet': # Need to create three different files for the probabilities of object and relationships
+    range_start_arr = [0, 10000, 20000]
+    range_end_arr = [10000, 20000, 26446]
+    save_file = 'data_rel_meas_infer_' + suffix + '.mat'
+    sio.savemat(save_file, {'measured_triplets':triplets_meas_list,'measured_box_list':boxes_list,'measured_label_list':labels_list,'measured_score_list':score_list,'measured_triplets_box_list':triplets_box_list,'measured_relations_list':relations_list})
 
-sio.savemat(save_file, {'measured_triplets':triplets_meas_list,'measured_box_list':boxes_list,'measured_label_list':labels_list,'measured_score_list':score_list,'measured_triplets_box_list':triplets_box_list,'measured_relations_list':relations_list,'predicate_logits_list':predicate_logits_list,'obj_logits_list':obj_logits_list})
+    for i in range(0, len(range_start_arr)):
+        range_start = range_start_arr[i]
+        range_end = range_end_arr[i]
+        save_file = 'data_rel_meas_infer_prob' + suffix + '_' + str(range_start+1) + '_' + str(range_end) + '.mat'
+        object_logits_list_short = obj_logits_list[range_start:range_end]
+        predicate_logits_list_short = predicate_logits_list[range_start:range_end]
+        sio.savemat(save_file, {'measured_triplets': triplets_meas_list, 'measured_box_list': boxes_list,
+                                'measured_label_list': labels_list, 'measured_score_list': score_list,
+                                'measured_triplets_box_list': triplets_box_list,
+                                'measured_relations_list': relations_list,
+                                'predicate_logits_list': predicate_logits_list_short, 'obj_logits_list': object_logits_list_short})
+
+if setting == 'sgcls': # Can save in single MATLAB file
+    save_file = 'data_rel_meas_infer_' + suffix + '.mat'
+    sio.savemat(save_file, {'measured_triplets':triplets_meas_list,'measured_box_list':boxes_list,
+                            'measured_label_list':labels_list,'measured_score_list':score_list,
+                            'measured_triplets_box_list':triplets_box_list,
+                            'measured_relations_list':relations_list,
+                            'predicate_logits_list':predicate_logits_list,'obj_logits_list':obj_logits_list})
+
+if setting == 'predcls': # Can save in single MATLAB file. No need to save object logits
+    save_file = 'data_rel_meas_infer_' + suffix + '.mat'
+    sio.savemat(save_file, {'measured_triplets':triplets_meas_list,'measured_box_list':boxes_list,
+                            'measured_label_list':labels_list,'measured_triplets_box_list':triplets_box_list,
+                            'measured_relations_list':relations_list,'predicate_logits_list':predicate_logits_list})
